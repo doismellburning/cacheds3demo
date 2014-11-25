5558b71d0b57bb2daf6faf5f0b95ca7572c36bda
Kristian Glass <git@doismellburning.co.uk>
2014-11-25 16:01:43 +0000

    Added core app, with favicon + redirect

--- cacheds3demo/cacheds3demo/urls.py
+++ cacheds3demo/cacheds3demo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cacheds3demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url('', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
